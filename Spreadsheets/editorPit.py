ecf3669346b43d4f976e0aeb5799bf6627f1acf9
Vítor Carvalho <vitohrs@gmail.com>
2023-04-13 23:35:13 -0300

    Módulo de planilhas Pit

--- Spreadsheets/editorPit.py
+++ Spreadsheets/editorPit.py
@@ -1,29 +1,118 @@
-from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
-
-wb = load_workbook('Anexo 2 - Planilha RIT.xlsx')
-ws = wb.active
-ws.title = 'Rit'
+from concurrent import futures
+import grpc, sheetsEditor_pb2_grpc, sheetsEditor_pb2, googleapi, random
+from database import sheets_pb2, sheets_pb2_grpc 
 
 
+channel = grpc.insecure_channel('26.52.183.15:6924')
+stub = sheets_pb2_grpc.SpreadsheetServiceStub(channel)
 
-#FUNÇÃO CRIAR RIT
+(DRIVE_CLIENT,SPREADSHEET_CLIENT) = googleapi.getClients()
 
-def dadosCabecalho():
-    ws['C3'].value = ''
-    ws['C4'].value = ''
-    ws['C5'].value = ''
-    ws['C6'].value = ''
-    ws['C7'].value = ''
-    ws['C8'].value = ''
-    ws['C9'].value = ''
+class SheetsService(sheetsEditor_pb2_grpc.SheetsServiceServicer):
 
-dadosCabecalho()
+    def GetPitPDF(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+    
+        getUser1 = sheets_pb2.GetUserRequest(id= request.userId)
+    
+        user = stub.GetUser(getUser1)
+       
+        getSheet1 = sheets_pb2.GetSheetRequest(userId=request.userId, type='pit', ano= request.ano)
 
-wb.save('Anexo 2 - Planilha RIT.xlsx')
+        sheet = stub.GetSheet(getSheet1)
+
+        sheetId = googleapi.copy_file(drive_client=DRIVE_CLIENT,file_id=googleapi.PLANILHA_ID,fileName=f'Planilha Pit - {user.name}',folder_id=googleapi.FOLDER_ID)
+
+        popularPlanilha(spreadsheetclient=SPREADSHEET_CLIENT,ano = request.ano, user = user, sheet=sheet, fileId=sheetId)
+
+        pdf = googleapi.export_as_pdf(drive_client = DRIVE_CLIENT, doc_id = sheetId)
+
+        DRIVE_CLIENT.files().delete(fileId=sheetId).execute()
+
+        return sheetsEditor_pb2.PitAsPDF(fileName=f'PIT {request.ano}',pdf=pdf)
+
+def iniciarServidor():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    sheetsEditor_pb2_grpc.add_SheetsServiceServicer_to_server(
+      SheetsService(), server)
+    server.add_insecure_port('[::]:50053')
+    server.start()
+    server.wait_for_termination()
+    
+
+
+def popularPlanilha(spreadsheetclient, ano, user, sheet, fileId):
+    ensino = list(sheet.ensino)
+    ensino.append(sheet.ch_ensino)
+
+    pesquisa = list(sheet.pesquisa)
+    pesquisa.append(sheet.ch_pesquisa)
+
+    extensao = list(sheet.extensao)
+    extensao.append(sheet.ch_extensao)
+
+    adm = list(sheet.adm)
+    adm.append(sheet.ch_adm)
+
+    print(sheet.ch_grad) 
+    print(sheet.ch_pos)
+
+    body = {
+        "valueInputOption": "USER_ENTERED",
+        "data": [{
+            'range': "C2:C9",
+            'majorDimension': "COLUMNS",
+            'values': [[
+                ano,
+                user.name,
+                user.siape,
+                user.departamento,
+                user.email,
+                user.vinculo,
+                user.regime,
+                user.reducao
+            ]]},
+            {
+                'range': "I13:I14",
+                'majorDimension': "COLUMNS",
+                'values': [[
+                    sheet.ch_grad,
+                    sheet.ch_pos
+                ]]
+        },
+            {
+                'range': "I19:I35",
+                'majorDimension': "COLUMNS",
+                'values': [
+                    ensino
+                ]
+        },
+            {
+                'range': "I39:I68",
+                'majorDimension': "COLUMNS",
+                'values': [
+                    pesquisa
+                ]
+        },
+            {
+                'range': "I72:I90",
+                'majorDimension': "COLUMNS",
+                'values': [
+                    extensao
+                ]
+        },
+            {
+                'range': "I94:I105",
+                'majorDimension': "COLUMNS",
+                'values': [
+                    adm
+                ]
+        }
+        ]
+    }
+    spreadsheetclient.spreadsheets().values().batchUpdate(spreadsheetId=fileId, body=body).execute()
 
 
 
-
-  
+if __name__=='__main__':
+    iniciarServidor()
---
+++ Spreadsheets/googleapi.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/spreadsheets']
+
+FOLDER_ID = '1Llwkw-E59cqfln2NCIz3bJgwWezFUVKe'
+# The ID of a sample document.
+PLANILHA_ID = '1e6kvNSLUHF1VfWg7Jvqmroc2zJi2J2vQHrcD0_oz128'
+
+def export_as_pdf(drive_client, doc_id):
+    """
+    Exporta um documento como PDF e move para uma pasta (folder_id)
+    """
+    name = drive_client.files().get(fileId=doc_id).execute()['name']
+    name += '.pdf'
+
+    data =drive_client.files().export(fileId=doc_id,
+                                       mimeType='application/pdf').execute()
+    return data
+
+def copy_file(drive_client, file_id, fileName, folder_id):
+    """
+    Copia um arquivo para uma pasta
+    """
+    newFileId = drive_client.files().copy(fileId=file_id, body={'name': fileName}).execute().get('id')
+    file = drive_client.files().get(fileId=newFileId, fields='parents').execute()
+    previous_parents = ','.join(file.get('parents'))
+    drive_client.files().update(fileId=file_id,
+                                       addParents=folder_id,
+                                       removeParents=previous_parents,
+                                       fields='id, parents').execute()
+    return newFileId
+
+def getClients():
+
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        driveService = build('drive', 'v3',credentials=creds)
+        sheetService = build('sheets','v4',credentials=creds)
+        return driveService,sheetService
+    
+    except HttpError as err:
+        print(err)
